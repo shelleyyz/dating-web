72da8e316bda65d2e495a73769ad85dbd6a27df1
Zabrina L <zabrinatan@Zabrinas-MacBook-Pro.local>
2018-08-03 14:41:34 +1000

    created controllers

---
+++ app/controllers/mailboxes_controller.rb
@@ -0,0 +1,10 @@
+class MailboxesController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  get 'mailboxes/index'
+  get 'mailboxes/new'
+  get 'mailboxes/show'
+  get 'relationships/index'
+  get 'relationships/show'
+  get 'relationships/new'
+  get 'users/index'
+  get 'users/new'
+  get 'users/edit'
+  get 'users/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
