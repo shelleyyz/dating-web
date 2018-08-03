0af4065d3583207103644e542176b4717d0382e2
Sherine <sherinemay79@gmail.com>
2018-08-03 14:10:22 +1000

    initial commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_03_034214) do
+ActiveRecord::Schema.define(version: 2018_08_03_034459) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,9 @@ ActiveRecord::Schema.define(version: 2018_08_03_034214) do
     t.boolean "active"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "relationships_users", force: :cascade do |t|
   end
 
   create_table "users", force: :cascade do |t|
