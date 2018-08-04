d998f5e01c18fd2223fb624dd9cf9999739a4757
Shelley Zhang <shelleyyz91@gmail.com>
2018-08-04 14:31:09 +1000

    fixed seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,50 +5,49 @@ user1 = User.create(
   :first_name => "Shelley",
   :last_name => "Zhang",
   :password => "chicken",
-  :interests => "edit these!",
+  # :interests => "edit these!",
   :email => "shelley@ga.co",
-  :gender => "female",
-  :dob => '1991-11-04',
-  :location => "Sydney",
-  :bio => "Edit this",
-  :admin => true
+  # :gender => "female",
+  # :dob => '1991-11-04',
+  # :location => "Sydney",
+  # :bio => "Edit this",
+
 )
 
 user2 = User.create(
   :first_name => "Sherine",
   :last_name => "Foo",
   :password => "chicken",
-  :interests => "edit these!",
+  # :interests => "edit these!",
   :email => "sherine@ga.co",
-  :gender => "female",
-  :dob => '1979-06-05',
-  :location => "Sydney",
-  :bio => "Edit this",
-  :admin => true
+  # :gender => "female",
+  # :dob => '1979-06-05',
+  # :location => "Sydney",
+  # :bio => "Edit this",
+
 )
 
 user3 = User.create(
   :first_name => "Zabrina",
   :last_name => "Lagamayo",
   :password => "chicken",
-  :interests => "edit these!",
+  # :interests => "edit these!",
   :email => "zabrina@ga.co",
-  :gender => "female",
-  :dob => '1993-08-29',
-  :location => "Sydney",
-  :bio => "Edit this",
-  :admin => true
+  # :gender => "female",
+  # :dob => '1993-08-29',
+  # :location => "Sydney",
+  # :bio => "Edit this",
 )
 
 user4 = User.create(
   :first_name => "EJ",
   :last_name => "Willard",
   :password => "chicken",
-  :interests => "Japan",
+  # :interests => "Japan",
   :email => "ej@ga.co",
-  :gender => "female",
-  :dob => '1987-01-15',
-  :location => "Sydney",
-  :bio => "Don't worry - I'm not crazy about me either.",
-  :admin => true
+  # :gender => "female",
+  # :dob => '1987-01-15',
+  # :location => "Sydney",
+  # :bio => "Don't worry - I'm not crazy about me either.",
+
 )
