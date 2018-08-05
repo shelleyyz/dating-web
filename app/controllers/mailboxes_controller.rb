196a6d0b57b699b49fa014b80f94c60501f6dc6d
Zabrina L <zabrinatan@Zabrinas-MacBook-Pro.local>
2018-08-05 15:59:46 +1000

    got the conversations index to show receiver_id

--- app/controllers/mailboxes_controller.rb
+++ app/controllers/mailboxes_controller.rb
@@ -13,7 +13,7 @@ class MailboxesController < ApplicationController
     mailbox = Mailbox.new mailbox_params
     conversation = Conversation.new
     conversation.sender_id = @current_user.id
-    conversation.receiver_id = params[:id]
+    conversation.receiver_id = extra_params[:receiver_id].to_i
     conversation.save
     mailbox.conversation_id = conversation.id
     mailbox.content = mailbox_params[:content]
@@ -31,6 +31,9 @@ class MailboxesController < ApplicationController
   private
   def mailbox_params
     params.require(:mailbox).permit(:content, :sender_id, :conversation_id)
+  end
+  def extra_params
+    params.require(:extra).permit(:receiver_id)
   end
 
 end
