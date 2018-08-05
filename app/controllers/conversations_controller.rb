1fe360b3e937d9ac679f2c6e984c20500af6ebd9
Zabrina L <zabrinatan@Zabrinas-MacBook-Pro.local>
2018-08-05 23:15:36 +1000

    changed layout of mailbox index to table

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -2,6 +2,8 @@ class ConversationsController < ApplicationController
   def index
 
     @conversations = Conversation.where(:sender_id => @current_user.id).or(Conversation.where(:receiver_id => @current_user.id))
+
+
   end
 
   def new
