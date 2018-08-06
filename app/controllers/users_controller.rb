d5f4474d942199b65b347b4fc7bebd01df86e5a7
Shelley Zhang <shelleyyz91@gmail.com>
2018-08-06 10:19:59 +1000

    merging

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -32,7 +32,6 @@ class UsersController < ApplicationController
   def update
     @user = User.find params[:id]
     # @user.update user_params
-    binding.pry
     if @user.update (user_params)
     flash[:success] = "Profile updated"
     redirect_to @user
