18f152113bfacd793c780de396951f7f3009efd2
Sherine <sherinemay79@gmail.com>
2018-08-07 16:08:27 +1000

    merge

--- app/models/user.rb
+++ app/models/user.rb
@@ -30,15 +30,30 @@ class User < ApplicationRecord
   # validates :first_name, :presence => true
   # validates :last_name, :presence => true
 
-
-  # TODO: add dependent destroy to liking and likers
   has_many :liking, class_name: "Relationship", foreign_key: :likee_id, dependent: :destroy
   has_many :likers, class_name: "Relationship", foreign_key: :liker_id, dependent: :destroy
-
-  # has_many :active_relationships, ->(user) { where("relationships.liker_id = :user_id OR relationships.likee_id = :user_id", user_id: user.id) }
 
   has_many :mailboxes, :through =>:conversation
   has_many :conversations
   # enum language: [:javascript, :ruby, :python, :golang, :C, :php, :java]
+
+  # Users this user likes
+  def likes
+    self.likers.where(:liker_id => self.id).map do |r|
+      r.likee
+    end
+  end
+
+  # Users who like this user
+  def likees
+    self.liking.where(:likee_id => self.id).map do |r|
+      r.liker
+    end
+  end
+
+  # Users who like this user and vice versa
+  def matches
+    self.likes & self.likees # Intersection
+  end
 end
 end
