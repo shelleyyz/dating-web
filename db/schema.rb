ef567ad56044c82189b92e12a8ce55a4f8259e6f
ellijayne <ellijayne.m.willard@gmail.com>
2018-08-08 13:59:54 +1000

    fixed missing end error in user show

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_06_123700) do
+ActiveRecord::Schema.define(version: 2018_08_07_104620) do
 
-#These are extensions that must be enabled in order to support this database
+  # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "attachinary_files", id: :serial, force: :cascade do |t|
